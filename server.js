f5d92e19e3187b25144bd5e3d2daf3aaf32e89d4
Mohd Mohitur Rahaman <mohitur669@gmail.com>
2024-03-09 23:56:08 +0530

    path express

--- server.js
+++ server.js
@@ -10,6 +10,11 @@ const ACTIONS = require('./src/actions/Actions');
 const server = http.createServer(app);
 const io = new Server(server);
 
+app.use(express.static('build'));
+app.use((req, res, next) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
 const userSocketMap = {};
 function getAllConnectedClients(roomId) {
     // Map
